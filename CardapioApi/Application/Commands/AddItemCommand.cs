60844c8c3aac69ac02426c71a0d6cc60f7680f66
Diego Feijó <diego.feijo@hotmail.com>
2025-06-24 22:36:55 -0300

    feat: add item

---
+++ CardapioApi/Application/Commands/AddItemCommand.cs
@@ -0,0 +1,24 @@
+﻿using Application.Exceptions;
+using Application.Validators;
+using MediatR;
+
+namespace Application.Commands;
+public class AddItemCommand : IRequest<Guid>
+{
+    public string Nome { get; set; } = string.Empty;
+    public string Descricao { get; set; } = string.Empty;
+    public string NomeCategoria { get; set; } = string.Empty;
+    public decimal Preco { get; set; }
+    public bool Disponivel { get; set; }
+
+    public void Validate()
+    {
+        var validator = new ModelItemValidator();
+        var result = validator.Validate(this);
+        if (!result.IsValid)
+        {
+            var errors = result.Errors.Select(e => e.ErrorMessage).ToList();
+            throw new ValidationException(errors);
+        }
+    }
+}
---
+++ CardapioApi/Application/Services/RabbitMqService.cs
@@ -0,0 +1,12 @@
+﻿using Domain;
+using MassTransit;
+
+namespace Application.Services;
+public class RabbitMqService(IBus bus) : IRabbitMqService
+{    
+    public async Task PublishAdd(AddItemDto message, string queue)
+    {
+        var endpoint = await bus.GetSendEndpoint(new Uri($"queue:{queue}"));
+        await endpoint.Send(message);
+    }
+}
